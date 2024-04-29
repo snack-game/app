b6311a069f380fbb5d4f24a39131d1d810f9f3fe
dev-dong-su <fkdlrj1230@gmail.com>
2024-04-30 03:00:23 +0900

    refactor: url 변경

--- src/components/constants/path.constants.ts
+++ src/components/constants/path.constants.ts
@@ -1,7 +1,6 @@
-export const BASEURL = 'http://localhost:4000';
+export const BASEURL = 'http://dev.snackga.me';
+// export const BASEURL = 'http://localhost:4000';
 export const PATH = {
   AUTH: BASEURL + '/auth',
   GAME: BASEURL + '/snack-game',
 };
-
-// export const BASEURL = 'http://dev.snackga.me'
